9b51584b02d02bd7f83804e137bc01669eabf17b
victorviterbo <vic.viterbo@gmail.com>
2026-02-03 16:17:05 +0100

    rebasing with new front

--- services/backend/project/settings.py
+++ services/backend/project/settings.py
@@ -14,7 +14,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # During dev, used to store user's media
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'DB/media')
 
 MEDIA_URL = '/DB/media/'
 
