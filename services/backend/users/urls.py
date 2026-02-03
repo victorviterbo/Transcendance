6d0f20d17eeb8bb2c3a6fbc5e6e34b2a330f5cec
victorviterbo <vic.viterbo@gmail.com>
2026-02-03 16:17:05 +0100

    rebasing with new front

--- services/backend/users/urls.py
+++ services/backend/users/urls.py
@@ -9,9 +9,9 @@ from .views import (
 from django.urls import path
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='user-login'),
-    path('register/', RegisterView.as_view(), name='user-register'),
-    path('profile/', ProfileView.as_view(), name="user-profile"),
-    path('logout/', LogoutView.as_view(), name="user-logout"),
-    path('refresh-token/', RefreshTokenView.as_view(), name="user-refresh-token"),
+    path('login', LoginView.as_view(), name='user-login'),
+    path('register', RegisterView.as_view(), name='user-register'),
+    path('profile', ProfileView.as_view(), name="user-profile"),
+    path('logout', LogoutView.as_view(), name="user-logout"),
+    path('refresh-token', RefreshTokenView.as_view(), name="user-refresh-token"),
 ]
