768f479574039a64429c28ffed51b8e0d973afaf
Sarah Gauthier <carlmc.kayre@gmail.com>
2026-02-21 18:21:57 +0100

    WORK: Rooms lists display

---
+++ services/frontend/src/components/inputs/buttons/CButtonRoom.tsx
@@ -0,0 +1,21 @@
+import { Stack } from "@mui/material";
+import type { IRoomInfo } from "../../../types/room";
+import type { CButtonProps } from "./CButton";
+import CButton from "./CButton";
+import CText from "../../text/CText";
+
+interface CButtonRoomProps extends CButtonProps {
+	infos: IRoomInfo
+}
+
+function CButtonRoom({infos, ...other}: CButtonRoomProps) {
+	return <CButton sx={{width: 175, height: 175}} {...other}>
+		<Stack>
+			<CText size="md">{infos.name}</CText>
+			<CText size="md">{infos.theme}</CText>
+			<CText size="md">{infos.playerCount + " / " + infos.playerMax}</CText>
+		</Stack>
+	</CButton>
+}
+
+export default CButtonRoom;
