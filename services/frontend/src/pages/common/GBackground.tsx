69806961b2c2400b09db6f9d89574c5de30bad1b
Sarah Gauthier <carlmc.kayre@gmail.com>
2026-03-03 09:20:46 +0100

    WORK: BG Cartoon

--- services/frontend/src/pages/common/GBackground.tsx
+++ services/frontend/src/pages/common/GBackground.tsx
@@ -1,6 +1,8 @@
 import { Box } from "@mui/material";
 import { appThemeDef } from "../../styles/theme";
 const bgWood = "imgs/shared/bg_wood_2.png";
+const bgPolar = "imgs/shared/BGPolarMask.png";
+const bgIcons = "imgs/shared/BG_Icons.png";
 
 function GBackground() {
 	return (
@@ -19,14 +21,71 @@ function GBackground() {
 
 					height: "100%",
 					width: "100%",
-					zIndex: -1,
+					zIndex: -3,
 
-					backgroundImage: `url(${bgWood})`,
-					backgroundSize: "10%",
-
-					filter: `brightness(${appThemeDef.bg.brightness})`,
+					backgroundColor: "#5612f5",
 				}}
 			></Box>
+			<Box
+				sx={{
+					position: "absolute",
+
+					height: "100%",
+					width: "100%",
+					zIndex: 0,
+
+					backgroundColor: "#3d0d8a",
+					maskImage: `url("${bgIcons}")`,
+					maskSize: "50%",
+					maskPosition: "center",
+
+					WebkitMaskImage: `url("${bgIcons}")`,
+					WebkitMaskSize: "50%",
+					WebkitMaskPosition: "center",
+
+					filter: "drop-shadow(20px 20px 20px #000000)",
+				}}
+			></Box>
+			<Box
+				sx={{
+					position: "absolute",
+					inset: 0,
+					filter: "drop-shadow(20px 20px 12px rgba(0,0,0,0.8))",
+				}}
+			>
+				<Box
+					sx={{
+						position: "absolute",
+						inset: 0,
+						backgroundColor: "#5656f5",
+						WebkitMaskImage: `url(${bgPolar})`,
+						WebkitMaskRepeat: "no-repeat",
+						WebkitMaskSize: "100% 100%",
+						WebkitMaskPosition: "center",
+					}}
+				>
+					<Box
+						sx={{
+							position: "absolute",
+
+							height: "100%",
+							width: "100%",
+							zIndex: 0,
+
+							backgroundColor: "#1d19e6",
+							maskImage: `url("${bgIcons}")`,
+							maskSize: "50%",
+							maskPosition: "center",
+
+							WebkitMaskImage: `url("${bgIcons}")`,
+							WebkitMaskSize: "50%",
+							WebkitMaskPosition: "center",
+
+							filter: "drop-shadow(20px 20px 20px #000000)",
+						}}
+					></Box>
+				</Box>
+			</Box>
 		</Box>
 	);
 }
