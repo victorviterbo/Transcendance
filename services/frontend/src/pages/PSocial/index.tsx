9356b1ca061945d6624d226c46034e9707056d91
Sarah Gauthier <carlmc.kayre@gmail.com>
2026-03-24 12:14:34 +0100

    Error handled

--- services/frontend/src/pages/PSocial/index.tsx
+++ services/frontend/src/pages/PSocial/index.tsx
@@ -3,21 +3,26 @@ import CTabs from "../../components/navigation/CTabs";
 import CTitlePaper from "../../components/surfaces/CTitlePaper";
 import CTextField from "../../components/inputs/textFields/CTextField";
 import PFriendNode from "./PFriendNode";
-import { useEffect, useId, useState } from "react";
+import { useEffect, useId, useState, type ReactNode } from "react";
 import type { AxiosResponse } from "axios";
 import type { IFriendInfo, IFriendsList, TFriendStatus } from "../../types/friends";
 import api from "../../api";
 import { API_SOCIAL_FRIENDS } from "../../constants";
+import CText from "../../components/text/CText";
+import { getErrorNode } from "../../utils/error";
 
 function PSocial() {
 	const [friends, setFriends] = useState<IFriendInfo[]>([]);
 	const [friendsFilter, setFriendsFilter] = useState<string>("");
+	const [error, setError] = useState<ReactNode | undefined>(undefined);
 	const localId = useId();
 
 	useEffect(() => {
 		async function getFriends(): Promise<void> {
 			try {
 				const res: AxiosResponse<IFriendsList> = await api.get(API_SOCIAL_FRIENDS);
+				if(res.data.error)
+					throw res.data.error;
 				if (typeof res.data != "object" || !res.data.friends) {
 					console.error("Failed to load friends: 'Unknown'");
 					setFriends([]);
@@ -45,11 +50,32 @@ function PSocial() {
 				setFriends(res.data.friends);
 			} catch (error) {
 				console.error("Failed to load friends: ", error);
+				setError(getErrorNode(error, "FRIEND_ERROR"));
 				setFriends([]);
 			}
 		}
 		getFriends();
-	}, [setFriends]);
+	}, [setFriends, setError]);
+
+	function getFriendsList(): ReactNode | ReactNode[] {
+		if (error) return error;
+
+		if (friends.length == 0) return <CText align="center">FRIEND_EMPTY</CText>;
+		return friends.map((value: IFriendInfo, index: number) => {
+			return (
+				<PFriendNode
+					user={value}
+					key={localId + index}
+					hidden={
+						friendsFilter != "" &&
+						!value.username
+							.toLocaleLowerCase()
+							.includes(friendsFilter.toLocaleLowerCase())
+					}
+				></PFriendNode>
+			);
+		});
+	}
 
 	return (
 		<CTitlePaper
@@ -74,20 +100,7 @@ function PSocial() {
 					></CTextField>
 					<Box sx={{ mt: "20px", flex: 1, overflowY: "auto" }}>
 						<Stack sx={{ mt: "20px", flex: 1, overflowY: "auto" }}>
-							{friends.map((value: IFriendInfo, index: number) => {
-								return (
-									<PFriendNode
-										user={value}
-										key={localId + index}
-										hidden={
-											friendsFilter != "" &&
-											!value.username
-												.toLocaleLowerCase()
-												.includes(friendsFilter.toLocaleLowerCase())
-										}
-									></PFriendNode>
-								);
-							})}
+							{getFriendsList()}
 						</Stack>
 					</Box>
 				</Stack>
