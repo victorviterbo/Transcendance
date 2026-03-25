4c23856be3738851664c15e20ac5f2f34eab5494
victorviterbo <vic.viterbo@gmail.com>
2026-03-25 16:17:28 +0100

    FIX(2): forgot to add some files to previous commit

--- services/backend/friends/apps.py
+++ services/backend/friends/apps.py
@@ -4,5 +4,6 @@ from django.apps import AppConfig
 
 
 class FriendsConfig(AppConfig):
+    """Define linking of social module to the rest of the backend."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'friends'
