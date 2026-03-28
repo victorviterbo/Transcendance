8fe88aa0ae169a6963d8b46fdd4f8f5ff21a34d6
Sarah Gauthier <carlmc.kayre@gmail.com>
2026-03-28 18:15:19 +0100

    WORK: INcoming and Outgoing design done
    !!!! Test are not valids and will be fixed in next push

--- services/frontend/src/mock/handlers/social.ts
+++ services/frontend/src/mock/handlers/social.ts
@@ -1,6 +1,15 @@
 import { http, HttpResponse } from "msw";
-import { API_SOCIAL_FRIENDS, API_SOCIAL_FRIENDS_SEARCH } from "../../constants";
-import type { IFriendInfo, TFriendStatus } from "../../types/friends";
+import {
+	API_SOCIAL_FRIENDS,
+	API_SOCIAL_FRIENDS_REQUEST,
+	API_SOCIAL_FRIENDS_SEARCH,
+} from "../../constants";
+import type {
+	IFriendInfo,
+	IFriendRequests,
+	TFriendRelation,
+	TFriendStatus,
+} from "../../types/friends";
 import { mockProfilesPics } from "../rcs/profilepics";
 import type { IExtUserInfo, IExtUserList, IExtUserSearch } from "../../types/user";
 
@@ -37,11 +46,11 @@ export function mockGenerateFriend(): IFriendInfo {
 		created_at: new Date().toString(),
 	};
 }
+
 export function mockGetExtUser(index: number): IExtUserInfo {
 	const userList: IExtUserList = mockGetExtUsers("");
 	return userList.users[index];
 }
-
 export function mockGetExtUsers(searchFilter: string): IExtUserList {
 	const usernames = [
 		"Sarah",
@@ -57,6 +66,7 @@ export function mockGetExtUsers(searchFilter: string): IExtUserList {
 		"SdS",
 	];
 	const badges = ["The mask singer", "Pro gesser", "Diva", "DJ", "casual gamer", "Guess master"];
+	const relation: TFriendRelation[] = ["not-friends", "incoming", "outgoing"];
 
 	const list: IExtUserList = { users: [] };
 	usernames.forEach((name: string, index: number) => {
@@ -71,10 +81,14 @@ export function mockGetExtUsers(searchFilter: string): IExtUserList {
 			username: name,
 			image: mockProfilesPics[index % mockProfilesPics.length],
 			badges: badges[index % badges.length],
+			relation: relation[index % relation.length],
 		});
 	});
 
 	return list;
+}
+export function mockGetMaxUsers(): number {
+	return 11;
 }
 
 //--------------------------------------------------
@@ -122,4 +136,32 @@ export const friendsSearchHandler = http.post(API_SOCIAL_FRIENDS_SEARCH, async (
 			{ status: isError ? 400 : 200 },
 		);
 	return HttpResponse.json(mockGetExtUsers(data.search));
+});
+
+export const friendsRequestsHandler = http.get(API_SOCIAL_FRIENDS_REQUEST, async () => {
+	const isError: boolean = false;
+
+	const res: IFriendRequests = {
+		outgoing: [],
+		incoming: [],
+	};
+
+	const displayed = Math.floor(Math.random() * 7);
+
+	for (let i = 0; i < displayed; i++) {
+		if (Math.round(Math.random())) {
+			res.outgoing.push(mockGetExtUser(i % mockGetMaxUsers()));
+			res.outgoing[res.outgoing.length - 1].relation = "outgoing";
+		} else {
+			res.incoming.push(mockGetExtUser(i % mockGetMaxUsers()));
+			res.incoming[res.incoming.length - 1].relation = "incoming";
+		}
+	}
+
+	if (isError) {
+		res.error = {
+			default: [{ message: "Friends are disabled", code: "Can't look up for friends" }],
+		};
+	}
+	return HttpResponse.json(res, { status: isError ? 400 : 200 });
 });
