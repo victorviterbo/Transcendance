0c1705dfeafc9ef26717042215f9b4aa5cee2b4b
Ego8316 <fuusukew@gmail.com>
2026-03-31 19:31:24 +0200

    [feature] add 404 page

--- services/frontend/src/pages/static/PNotFound.tsx
+++ services/frontend/src/pages/static/PNotFound.tsx
@@ -1,7 +1,18 @@
+import CTitlePaper from "../../components/surfaces/CTitlePaper";
+import { Container } from "@mui/material";
 import GPageBase from "../common/GPageBases";
+import CText from "../../components/text/CText";
 
 const PNotFound = () => {
-	return <GPageBase>404</GPageBase>;
+	return (
+		<GPageBase>
+			<Container sx={{ mt: "5%" }} maxWidth="sm">
+				<CTitlePaper title="404" titleType="title" titleSize="lg">
+					<CText>NOT_FOUND</CText>
+				</CTitlePaper>
+			</Container>
+		</GPageBase>
+	);
 };
 
 export default PNotFound;
