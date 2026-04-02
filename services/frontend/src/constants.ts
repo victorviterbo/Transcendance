991406364f3ff2f0ddcaa2babeec3a85232820cb
victorviterbo <vic.viterbo@gmail.com>
2026-04-02 12:14:59 +0200

    FIX: forgot to include changes in frontend

--- services/frontend/src/constants.ts
+++ services/frontend/src/constants.ts
@@ -2,6 +2,7 @@ export const API_AUTH_REFRESH = "/api/auth/refresh/";
 export const API_AUTH_LOGOUT = "/api/auth/logout/";
 export const API_AUTH_LOGIN = "/api/auth/login/";
 export const API_AUTH_REGISTER = "/api/auth/register/";
+export const API_ACCOUNT_DELETE = "/api/auth/delete/";
 export const API_PROFILE_PASSWORD = "/api/auth/password/";
 
 export const API_PROFILE = "/api/profile/";
