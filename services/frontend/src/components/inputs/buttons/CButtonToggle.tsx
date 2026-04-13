9135f2bb6390f47dca5e2ecf462216b9c4acd3f5
Sarah Gauthier <carlmc.kayre@gmail.com>
2026-04-13 18:55:07 +0200

    [WORK]: Added notif number on toggle button

--- services/frontend/src/components/inputs/buttons/CButtonToggle.tsx
+++ services/frontend/src/components/inputs/buttons/CButtonToggle.tsx
@@ -1,15 +1,35 @@
-import { ToggleButton, type ToggleButtonProps } from "@mui/material";
+import { Box, ToggleButton, type ToggleButtonProps } from "@mui/material";
 import type { GProps } from "../../common/GProps";
-import { CToggleButtonStyle } from "../../../styles/components/inputs/CToggleStyle";
+import {
+	CToggleButtonNotif,
+	CToggleButtonStyle,
+} from "../../../styles/components/inputs/CToggleStyle";
+import CText from "../../text/CText";
+import type { ReactNode } from "react";
 
-export interface CButtonToggleProps extends GProps, ToggleButtonProps {}
+export interface CButtonToggleProps extends GProps, ToggleButtonProps {
+	notifCount?: number;
+}
 
-function CButtonToggle({ sx, ...other }: CButtonToggleProps) {
+function CButtonToggle({ notifCount, sx, ...other }: CButtonToggleProps) {
+	function getNotif(): ReactNode | undefined {
+		if (!notifCount) return;
+		if (notifCount <= 0) return;
+		return (
+			<Box sx={CToggleButtonNotif}>
+				<CText size="xs">{notifCount}</CText>
+			</Box>
+		);
+	}
+
 	return (
-		<ToggleButton
-			sx={[CToggleButtonStyle, ...(Array.isArray(sx) ? sx : sx ? [sx] : [])]}
-			{...other}
-		></ToggleButton>
+		<Box sx={{ position: "relative" }}>
+			{getNotif()}
+			<ToggleButton
+				sx={[CToggleButtonStyle, ...(Array.isArray(sx) ? sx : sx ? [sx] : [])]}
+				{...other}
+			></ToggleButton>
+		</Box>
 	);
 }
 
