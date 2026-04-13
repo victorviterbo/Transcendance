4ef711c050422b64955abf959c424abde1c513e9
Sarah Gauthier <carlmc.kayre@gmail.com>
2026-04-13 18:57:00 +0200

    [WORK]: Notif dawer added

---
+++ services/frontend/src/pages/PNotif/PNotifNode.tsx
@@ -0,0 +1,64 @@
+import { Box, Stack } from "@mui/material";
+import CText from "../../components/text/CText";
+import type { TNotif } from "../../types/socials";
+import type { GPageProps } from "../common/GPageBases";
+import { appTexts } from "../../styles/theme";
+import {
+	PNotifNodeImpText,
+	PNotifNodeSeeButton,
+	PNotifNodeStyle,
+} from "../../styles/pages/social/PNotifNodeStyle";
+import { ttrf } from "../../localization/localization";
+import { useCallback } from "react";
+import CIconButton from "../../components/inputs/buttons/CIconButton";
+import LaunchIcon from "@mui/icons-material/Launch";
+
+export interface PNotifNodeProps extends GPageProps {
+	notif: TNotif;
+	onSeeFriendsReq?: () => void;
+}
+
+function PNotifNode({ notif, onSeeFriendsReq }: PNotifNodeProps) {
+	const handleSee = useCallback(() => {
+		if (notif.kind == "friend-request" && onSeeFriendsReq) onSeeFriendsReq();
+	}, [notif, onSeeFriendsReq]);
+
+	const getAgo = useCallback((): string => {
+		const dateIn = typeof notif.date == "string" ? new Date(notif.date) : notif.date;
+		const timeSince: number = Date.now() - dateIn.getTime();
+		if (timeSince >= 1000 * 60 * 60 * 24)
+			return ttrf("NOTIF_AGO_DAYS", {
+				COUNT: Number(Math.trunc(timeSince / (1000 * 60 * 60 * 24))).toString(),
+			});
+		else if (timeSince >= 1000 * 60 * 60)
+			return ttrf("NOTIF_AGO_HOURS", {
+				COUNT: Number(Math.trunc(timeSince / (1000 * 60 * 60))).toString(),
+			});
+		else if (timeSince >= 1000 * 60)
+			return ttrf("NOTIF_AGO_MINUTES", {
+				COUNT: Number(Math.trunc(timeSince / (1000 * 60))).toString(),
+			});
+		return "NOTIF_AGO_LESS";
+	}, [notif]);
+
+	return (
+		<Box sx={(theme) => PNotifNodeStyle(theme, { notif })}>
+			<Stack direction={"row"}>
+				<Stack direction={"column"}>
+					<Stack direction={"row"}>
+						<CText>NOTIF_FRIEND_REQ</CText>
+						<CText sx={PNotifNodeImpText}>{notif.from.username}</CText>
+					</Stack>
+					<CText family={appTexts.text.secondaryFamily} size="xs" fontWeight={400}>
+						{getAgo()}
+					</CText>
+				</Stack>
+				<CIconButton onClick={handleSee} sx={PNotifNodeSeeButton}>
+					<LaunchIcon fontSize="small" />
+				</CIconButton>
+			</Stack>
+		</Box>
+	);
+}
+
+export default PNotifNode;
--- services/frontend/src/pages/common/GPageBases.tsx
+++ services/frontend/src/pages/common/GPageBases.tsx
@@ -9,6 +9,7 @@ import { useAuth } from "../../components/auth/CAuthProvider";
 import CFooter from "../../components/navigation/CFooter";
 import { cssAddSizes, sizeMakeString } from "../../utils/styles";
 import PSocial from "../PSocial";
+import PNotif from "../PNotif";
 
 export interface GPageProps extends GProps {
 	children?: ReactNode;
@@ -17,9 +18,24 @@ export interface GPageProps extends GProps {
 function GPageBase({ children }: GPageProps) {
 	const { user } = useAuth();
 	const [friendOpen, setFriendOpen] = useState<boolean>(false);
+	const [friendTab, setFriendTab] = useState<number>(0);
+	const [notifOpen, setNotifOpen] = useState<boolean>(false);
+	const [notifCount, setNotifCount] = useState<number>(0);
 
 	const handleOpenFriend = () => {
 		setFriendOpen(!friendOpen);
+		setNotifOpen(false);
+	};
+
+	const handleOpenNotif = () => {
+		setNotifOpen(!notifOpen);
+		setFriendOpen(false);
+	};
+
+	const handleOpenFriendRequests = () => {
+		setFriendOpen(true);
+		setNotifOpen(false);
+		setFriendTab(2);
 	};
 
 	return (
@@ -37,7 +53,13 @@ function GPageBase({ children }: GPageProps) {
 						alignItems: "stretch",
 					}}
 				>
-					<CNavbar onToggleFriend={handleOpenFriend} isFriendActive={friendOpen} />
+					<CNavbar
+						onToggleFriend={handleOpenFriend}
+						isFriendActive={friendOpen}
+						onToggleNotif={handleOpenNotif}
+						isNotifActive={notifOpen}
+						notifCount={notifCount}
+					/>
 					<Stack sx={{ flex: 1, overflowY: "auto", overflowX: "hidden" }}>
 						<Box
 							sx={(Theme) => ({
@@ -46,11 +68,12 @@ function GPageBase({ children }: GPageProps) {
 									duration: appAnimation.timing.enteringScreen,
 								}),
 								flex: 1,
-								width: friendOpen
-									? "calc( 100% - " +
-										sizeMakeString(appPositions.sizes.friends) +
-										")"
-									: "100%",
+								width:
+									friendOpen || notifOpen
+										? "calc( 100% - " +
+											sizeMakeString(appPositions.sizes.friends) +
+											")"
+										: "100%",
 							})}
 						>
 							{children}
@@ -72,7 +95,36 @@ function GPageBase({ children }: GPageProps) {
 							open={friendOpen}
 							data-testid="PSocialDrawer"
 						>
-							<PSocial></PSocial>
+							<PSocial
+								activeTab={friendTab}
+								onTabChanged={(value: number) => {
+									setFriendTab(value);
+								}}
+							></PSocial>
+						</CDrawer>
+					)}
+					{user && (
+						<CDrawer
+							width={appPositions.sizes.friends}
+							margin={{
+								top: cssAddSizes(
+									appPositions.sizes.header,
+									appPositions.socialMargin?.top,
+								),
+								right: appPositions.socialMargin?.right,
+								bottom: appPositions.socialMargin?.bottom,
+								left: appPositions.socialMargin?.left,
+							}}
+							open={notifOpen}
+							data-testid="PNotifDrawer"
+						>
+							<PNotif
+								isOpen={notifOpen}
+								onSeeFriendsReq={handleOpenFriendRequests}
+								onNotifCount={(value: number) => {
+									setNotifCount(value);
+								}}
+							></PNotif>
 						</CDrawer>
 					)}
 				</Stack>
