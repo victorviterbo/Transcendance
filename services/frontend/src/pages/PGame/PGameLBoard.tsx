8916d96c94c77a42c091d5c73be9923432d13131
kgauthie <kgauthie@student.42lausanne.ch>
2026-04-28 16:02:19 +0200

    [WORK]: Leaderboard started

--- services/frontend/src/pages/PGame/PGameLBoard.tsx
+++ services/frontend/src/pages/PGame/PGameLBoard.tsx
@@ -1,7 +1,11 @@
 import CGamePaper from "../../components/surfaces/CGamePaper";
 
+
+
 function PGameLBoard() {
-	return <CGamePaper title={"GAME_LEADER_BOARD"}> </CGamePaper>;
+	return <CGamePaper title={"GAME_LEADER_BOARD"}>
+
+	</CGamePaper>;
 }
 
 export default PGameLBoard;
