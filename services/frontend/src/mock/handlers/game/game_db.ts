9a26be6eb66c7f4c2cb328c0318f4bb72f6ddc47
Sarah Gauthier <carlmc.kayre@gmail.com>
2026-04-30 12:33:51 +0200

    [MOCK]: Preadding player to the game

--- services/frontend/src/mock/handlers/game/game_db.ts
+++ services/frontend/src/mock/handlers/game/game_db.ts
@@ -1,4 +1,5 @@
 import type { IGameData } from "../../../types/game";
+import { mockSocialDB, mockSocialSetDB } from "../social/social_dbs";
 
 export interface IMockGameData extends IGameData {
 
@@ -8,10 +9,23 @@ const mockGameData: Record<string, IMockGameData> = {}
 
 export function mockCreateRoom(RoomID: string) {
 	
+	mockSocialSetDB();
+
 	const nRoom: IMockGameData = {
 		players: [],
 		maxPlayers: 100
 	}
+
+	//Adding already presents players
+	for(let i = 0; i < 4; i++)
+	{
+		nRoom.players.push({
+			points: 0,
+			user: mockSocialDB.users[i],
+		})
+	}
+
+
 	mockGameData[RoomID] = nRoom;
 	return nRoom;
 
