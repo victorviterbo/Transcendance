0b741cad1a7ab1a99a04d7fac4e96c503987fcbc
Sarah Gauthier <carlmc.kayre@gmail.com>
2026-05-03 11:34:25 +0200

    [MOCK]: Add initia mchat in mock

--- services/frontend/src/types/game.ts
+++ services/frontend/src/types/game.ts
@@ -1,13 +1,29 @@
 import type { IErrorStruct } from "./error";
 import type { IExtUserInfo } from "./user";
 
+//====================== CHAT ======================
+export type TGameChatType = "message" | "joined" | "leaved" | "guessed" | "found";
+
+export interface IGameChatMsg {
+	userid: string;
+	username: string;
+	uid: string;
+
+	type: TGameChatType;
+	message?: string;
+}
+
+//====================== PLAYER ======================
 export interface IGamePlayer {
 	user: IExtUserInfo;
 	points: number;
 	host: boolean;
+	colorid: number;
 }
 
+//====================== COMMON ======================
 export interface IGameData {
+	chat: IGameChatMsg[];
 	players: IGamePlayer[];
 	maxPlayers: number;
 	isHost: boolean;
