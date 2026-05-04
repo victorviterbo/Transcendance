96884843a70ffea0447e54b44c2c8fe212b019fe
Sarah Gauthier <carlmc.kayre@gmail.com>
2026-05-04 11:53:13 +0200

    [WORK]: Chat styling and structure DONE

--- services/frontend/src/pages/PGame/PGameChatNode.tsx
+++ services/frontend/src/pages/PGame/PGameChatNode.tsx
@@ -1,10 +1,11 @@
-import { Stack } from "@mui/material";
+import { Box } from "@mui/material";
 import { type IGameChatMsg, type IGamePlayer } from "../../types/game";
 import type { GPageProps } from "../common/GPageBases";
 import CText from "../../components/text/CText";
 import { colorFromID } from "../../utils/styles";
-import { appTexts } from "../../styles/theme";
+import { appColors, appTexts } from "../../styles/theme";
 import { PGameChatNodeStyle } from "../../styles/pages/game/PGameChatStyle";
+import { ttrfn } from "../../localization/localization";
 
 interface PGameChatNodeProps extends GPageProps {
 	message: IGameChatMsg;
@@ -15,17 +16,66 @@ function PGameChatNode({ message, user }: PGameChatNodeProps) {
 	//====================== RETURS ======================
 	if (message.type == "message" && message.message) {
 		return (
-			<Stack direction={"row"} sx={PGameChatNodeStyle()}>
+			<Box sx={PGameChatNodeStyle()}>
 				<CText
 					noTr={true}
 					family={appTexts.text.secondaryFamily}
 					fontWeight={600}
 					size="md"
 				>
-					<span style={{ color: colorFromID(user.colorid) }}>{user.user.username}</span>:{" "}
+					<span style={{ color: colorFromID(user.colorid), fontWeight: 700}}>{user.user.username}</span>:{" "}
 					{message.message}
 				</CText>
-			</Stack>
+			</Box>
+		);
+	} else if (message.type == "joined" || message.type == "leaved") {
+		return (
+			<Box sx={PGameChatNodeStyle()}>
+				<CText
+					sx={{color: appColors.greys[3]}}
+					noTr={true}
+					family={appTexts.text.secondaryFamily}
+					fontWeight={600}
+					size="md"
+				>
+					{ttrfn(message.type  == "joined" ? "GAME_JOINED_MESSAGE" : "GAME_LEAVED_MESSAGE", {
+						PLAYER: <span style={{ color: colorFromID(user.colorid), fontWeight: 700}}>{user.user.username}</span>,
+					})}
+				</CText>
+			</Box>
+		);
+	} else if (message.type == "guessed" && message.message) {
+		return (
+			<Box sx={PGameChatNodeStyle()}>
+				<CText
+					sx={{color: appColors.greys[3]}}
+					noTr={true}
+					family={appTexts.text.secondaryFamily}
+					fontWeight={600}
+					size="md"
+				>
+					{ttrfn("GAME_GUESSED_MESSAGE", {
+						PLAYER: <span style={{ color: colorFromID(user.colorid), fontWeight: 700}}>{user.user.username}</span>,
+						GUESS: <span style={{ color: appColors.cancel[1], fontWeight: 500}}>{message.message}</span>,
+					})}
+				</CText>
+			</Box>
+		);
+	} else if (message.type == "found") {
+		return (
+			<Box sx={PGameChatNodeStyle()}>
+				<CText
+					sx={{color: appColors.validate[1]}}
+					noTr={true}
+					family={appTexts.text.secondaryFamily}
+					fontWeight={600}
+					size="md"
+				>
+					{ttrfn("GAME_FOUND_MESSAGE", {
+						PLAYER: <span style={{ color: colorFromID(user.colorid), fontWeight: 700}}>{user.user.username}</span>,
+					})}
+				</CText>
+			</Box>
 		);
 	}
 
