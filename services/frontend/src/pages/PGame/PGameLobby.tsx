37c5d91be4957097257311fa7c42ef0c59cd0774
Sarah Gauthier <carlmc.kayre@gmail.com>
2026-05-09 12:52:47 +0200

    [WORK]: Game lobby done

--- services/frontend/src/pages/PGame/PGameLobby.tsx
+++
@@ -1,7 +0,0 @@
-import CGamePaper from "../../components/surfaces/CGamePaper";
-
-function PGameLobby() {
-	return <CGamePaper title={"GAME_LOBBY_TITLE"}> </CGamePaper>;
-}
-
-export default PGameLobby;
---
+++ services/frontend/src/pages/PGame/PGameViews/PGameLobby.tsx
@@ -0,0 +1,80 @@
+import { Box, Stack } from "@mui/material";
+import type { IGameData, IGamePlayer } from "../../../types/game";
+import CTitle from "../../../components/text/CTitle";
+import CText from "../../../components/text/CText";
+import { ttrf, ttrfn, ttrn } from "../../../localization/localization";
+import { useMemo } from "react";
+import { colorFromID } from "../../../utils/styles";
+import { appColors } from "../../../styles/theme";
+import CButtonText from "../../../components/inputs/buttons/CButtonText";
+
+interface PGameLobbyProps {
+	game: IGameData;
+	players: IGamePlayer[];
+}
+
+function PGameLobby({ players, game }: PGameLobbyProps) {
+	const host: IGamePlayer | undefined = useMemo(() => {
+		const targetUser: IGamePlayer | undefined = players.find(
+			(player: IGamePlayer) => player.host,
+		);
+		if (!targetUser) return undefined;
+		return targetUser;
+	}, [players]);
+
+	return (
+		<Box data-testid="PGameLobby">
+			<Stack direction="column">
+				<CTitle
+					sx={{ color: appColors.secondary[0], mt: "20px", mb: 0 }}
+					noTr={true}
+					align="center"
+					size="xl"
+				>
+					{game.name}
+				</CTitle>
+				<CText sx={{ mt: 0, mb: 0 }} align="center" size="sm">
+					{"<THEMES>"}
+				</CText>
+			</Stack>
+			<Stack
+				direction="column"
+				sx={{
+					position: "absolute",
+					inset: 0,
+					alignItems: "center",
+					justifyContent: "center",
+				}}
+			>
+				{!game.isHost && (
+					<CText size="lg" align="center" testid="PGameLobby-Waiting">
+						{host
+							? ttrfn("GAME_WAITING_START", {
+									USER: (
+										<span
+											style={{ color: colorFromID(host ? host.colorid : 0) }}
+										>
+											{host ? host.user.username : ""}
+										</span>
+									),
+								})
+							: "GAME_WAITING_START_NO_HOST"}
+					</CText>
+				)}
+				{game.isHost && (
+					<>
+						<CButtonText sx={{ mb: "15px", minWidth: "150px" }}>GAME_START</CButtonText>
+						<CButtonText sx={{ mb: "15px", minWidth: "150px" }}>GAME_EDIT</CButtonText>
+					</>
+				)}
+				<CText align="center" size="sm">
+					{ttrf("GAME_PLAYER_COUNT", {
+						COUNT: ttrn(players.length),
+						MAX: ttrn(game.maxPlayers),
+					})}
+				</CText>
+			</Stack>
+		</Box>
+	);
+}
+export default PGameLobby;
