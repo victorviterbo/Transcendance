83f1c605ee84f2fe0eba67ece1c66d7e7ca519dc
Sarah Gauthier <carlmc.kayre@gmail.com>
2026-05-12 13:44:02 +0200

    [FIX] Make fixed

--- services/frontend/src/pages/PGame/PGameViews/PGameSettings.tsx
+++ services/frontend/src/pages/PGame/PGameViews/PGameSettings.tsx
@@ -35,7 +35,7 @@ interface PGameSettingsProps extends GPageProps {
 	onSettingsChanged: (newSettings: IGameSettings) => void;
 }
 
-function PGameSettings({ settings, onSettingsChanged }: PGameSettingsProps) {
+function PGameSettings({ settings }: PGameSettingsProps) {
 	//====================== STATS ======================
 	const [tags, setTags] = useState<Record<string, boolean>>(structuredClone(settings.tags));
 	const [nbSongs, setNBSongs] = useState<number>(settings.nbMusic);
@@ -68,26 +68,35 @@ function PGameSettings({ settings, onSettingsChanged }: PGameSettingsProps) {
 	}, [tags, setTags]);
 
 	//====================== LOCAL COMPONENTS ======================
-	const getSlider = (min: number, max: number, step: number, currentValue: number, label: string, signal: React.Dispatch<React.SetStateAction<number>>): ReactNode => {
-		return <>
-			<CText size="md">
-				{ttrfn(label, {
-					COUNT: <span style={{ color: appColors.primary[0] }}>{currentValue}</span>,
-				})}
-			</CText>
-			<CSlider
-				min={min}
-				max={max}
-				step={step}
-				value={currentValue}
-				marks
-				onChange={(_: Event, value: number | number[]) => {
-					if (Array.isArray(value) && value.length > 0) signal(value[0]);
-					else if (typeof value == "number") signal(value);
-				}}
-			></CSlider>
-		</>
-	}
+	const getSlider = (
+		min: number,
+		max: number,
+		step: number,
+		currentValue: number,
+		label: string,
+		signal: React.Dispatch<React.SetStateAction<number>>,
+	): ReactNode => {
+		return (
+			<>
+				<CText size="md">
+					{ttrfn(label, {
+						COUNT: <span style={{ color: appColors.primary[0] }}>{currentValue}</span>,
+					})}
+				</CText>
+				<CSlider
+					min={min}
+					max={max}
+					step={step}
+					value={currentValue}
+					marks
+					onChange={(_: Event, value: number | number[]) => {
+						if (Array.isArray(value) && value.length > 0) signal(value[0]);
+						else if (typeof value == "number") signal(value);
+					}}
+				></CSlider>
+			</>
+		);
+	};
 
 	return (
 		<Stack sx={{ position: "absolute", inset: "15px" }} direction={"row"}>
@@ -104,14 +113,35 @@ function PGameSettings({ settings, onSettingsChanged }: PGameSettingsProps) {
 				></CTextField>
 				<Stack sx={PGameSettingsTagListStyle}>{tagList}</Stack>
 			</Stack>
-			<Stack direction={"column"} sx={{ flex: 1, mr: "10px"}}>
+			<Stack direction={"column"} sx={{ flex: 1, mr: "10px" }}>
 				<CText size="lg" sx={{ mx: 0 }}>
 					GAME_SETTINGS_GAME
 				</CText>
 				<Box sx={PGameSettingsSplitter}></Box>
-				{getSlider(SETTINGS_NGSONGS_MIN, SETTINGS_NGSONGS_MAX, SETTINGS_NGSONGS_STEP, nbSongs, "GAME_SETTINGS_NB_MUSIC", setNBSongs)}
-				{getSlider(SETTINGS_SONG_DURATION_MIN, SETTINGS_SONG_DURATION_MAX, SETTINGS_SONG_DURATION_STEP, timer, "GAME_SETTINGS_MUSIC_TIMER", setTimer)}
-				{getSlider(SETTINGS_BREAK_DURATION_MIN, SETTINGS_BREAK_DURATION_MAX, SETTINGS_BREAK_DURATION_STEP, breakTimer, "GAME_SETTINGS_BREAK_TIMER", setBreakTimer)}
+				{getSlider(
+					SETTINGS_NGSONGS_MIN,
+					SETTINGS_NGSONGS_MAX,
+					SETTINGS_NGSONGS_STEP,
+					nbSongs,
+					"GAME_SETTINGS_NB_MUSIC",
+					setNBSongs,
+				)}
+				{getSlider(
+					SETTINGS_SONG_DURATION_MIN,
+					SETTINGS_SONG_DURATION_MAX,
+					SETTINGS_SONG_DURATION_STEP,
+					timer,
+					"GAME_SETTINGS_MUSIC_TIMER",
+					setTimer,
+				)}
+				{getSlider(
+					SETTINGS_BREAK_DURATION_MIN,
+					SETTINGS_BREAK_DURATION_MAX,
+					SETTINGS_BREAK_DURATION_STEP,
+					breakTimer,
+					"GAME_SETTINGS_BREAK_TIMER",
+					setBreakTimer,
+				)}
 				<Stack direction={"row"}>
 					<CText>GAME_SETTINGS_SEE_OTHERS</CText>
 					<CButtonToggle value={settings.seeOthers}></CButtonToggle>
