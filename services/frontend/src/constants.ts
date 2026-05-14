25b7311eeb1a9625a6869a3011ed68329e63e8ff
Ego8316 <fuusukew@gmail.com>
2026-05-14 15:54:27 +0200

    [feature] api contract for home page endpoints

--- services/frontend/src/constants.ts
+++ services/frontend/src/constants.ts
@@ -33,6 +33,9 @@ export const API_SOCIAL_NOTIFS = "/api/social/notifs";
 export const API_SOCIAL_NOTIFS_READ = "/api/social/notifs_read";
 
 //GAME
+export const API_CREATE_GAME = "/api/game/";
+export const API_GAME_FRIENDS = "/api/game/friends/";
+export const API_GAME_PUBLIC = "/api/game/public/";
 export const API_GAME = "/api/game/{ROOMID}";
 
 // WS
