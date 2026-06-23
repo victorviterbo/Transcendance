7fe6a33bf4563d33b8fbe00ca13e0512890c6c66
Ego8316 <fuusukew@gmail.com>
2026-06-23 22:16:13 +0200

    [fix] stop deleting database and making migrations on container startup

--- services/backend/build/init.sh
+++ services/backend/build/init.sh
@@ -4,10 +4,6 @@ set -eu
 
 mkdir -p /backend/DB/website
 
-rm -f /backend/DB/website/db.sqlite3
-find /backend -path "*/migrations/0*" -delete
-
-conda run -n backend python /backend/manage.py makemigrations
 conda run -n backend python /backend/manage.py migrate
 conda run -n backend python /backend/manage.py collectstatic --noinput
 
@@ -19,8 +15,6 @@ if [ "$APP_MODE" = "test" ]; then
     exec conda run --no-capture-output -n backend python /backend/manage.py test
     exit 0
 fi
-
-conda run -n backend bash -c "python /backend/manage.py shell < /backend/seed.py"
 
 echo "Starting Production Server..."
 exec conda run --no-capture-output -n backend daphne -b 0.0.0.0 -p 8000 project.asgi:application
