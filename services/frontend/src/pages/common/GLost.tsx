9ddc2fe75b59b1e60785d3320c447eec90fb32ef
Sarah Gauthier <carlmc.kayre@gmail.com>
2026-07-05 16:53:58 +0200

    [FIX]:Loading styling

--- services/frontend/src/pages/common/GLost.tsx
+++ services/frontend/src/pages/common/GLost.tsx
@@ -1,26 +1,29 @@
-import { Box } from "@mui/material";
+import { Box, ThemeProvider } from "@mui/material";
 import GBackground from "./GBackground";
 import CBasePaper from "../../components/surfaces/CBasePaper";
 import CText from "../../components/text/CText";
+import appTheme from "../../styles/theme";
 
 function GLost() {
 	return (
-		<Box sx={{ position: "fixed", inset: 0 }}>
-			<GBackground />
-			<CBasePaper
-				sx={{
-					position: "relative",
-					maxWidth: "500px",
-					maxHeight: "150px",
-					mt: "50px",
-					mx: "auto",
-				}}
-			>
-				<CText align="center" sx={{ my: "auto" }}>
-					CONN_LOST
-				</CText>
-			</CBasePaper>
-		</Box>
+		<ThemeProvider theme={appTheme}>
+			<Box sx={{ position: "fixed", inset: 0 }}>
+				<GBackground />
+				<CBasePaper
+					sx={{
+						position: "relative",
+						maxWidth: "500px",
+						maxHeight: "150px",
+						mt: "50px",
+						mx: "auto",
+					}}
+				>
+					<CText align="center" sx={{ my: "auto" }}>
+						CONN_LOST
+					</CText>
+				</CBasePaper>
+			</Box>
+		</ThemeProvider>
 	);
 }
 
