b5bc7e1df24b427cda95668c559f40fdc27bbd45
Sarah Gauthier <carlmc.kayre@gmail.com>
2026-07-06 16:42:19 +0200

    [FIX]: Mock error

--- services/frontend/src/mock/handlers/home.ts
+++ services/frontend/src/mock/handlers/home.ts
@@ -104,8 +104,7 @@ export const FetchPrivateRoom = http.get(API_GAME_FRIENDS, async () => {
 		uid: "ended-host",
 		name: "Ended Host room",
 		genres: [...MUSIC_TAGS],
-		playerCount: Math.floor(Math.random() * 100),
-		playerMax: 100,
+		players: generatePlayers(),
 	});
 
 	tempRooms.push({
