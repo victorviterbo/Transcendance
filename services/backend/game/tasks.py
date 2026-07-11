c87e01fcac29540c8b7ef87a4b0c7f1b429b940e
victorviterbo <vic.viterbo@gmail.com>
2026-07-11 15:28:11 +0200

    FEAT: included stale started game in reaper

--- services/backend/game/tasks.py
+++ services/backend/game/tasks.py
@@ -6,21 +6,23 @@ from asgiref.sync import async_to_sync
 from celery import shared_task
 from channels.layers import get_channel_layer
 from django.utils import timezone
-
+from django.db.models import Q
 from game.models import Game
 from game.ws_game_db_helpers import _delete_aborted_game
 from game.ws_game_shared import ACTIVE_GAMES
-
+from project.defaults import lobby_timeout, finished_timeout
 
 @shared_task
 def reap_foresaken_waiting_games() -> None:
     """Find every game that has been in the loby for over 15 minutes and delete them."""
-    cutoff = timezone.now() - timedelta(minutes=1)
-    print(f"Reaping games created before {cutoff}\n\n\n\n")
+    cutoff_lobby = timezone.now() - timedelta(minutes=lobby_timeout)
+    cutoff_finished = timezone.now() - timedelta(minutes=finished_timeout)
+
+    print(f"Reaping games created before {cutoff_lobby}\n\n\n\n")
     stale_games = Game.objects.filter(
-        status='waiting',
-        created_at__lte=cutoff
-    )
+    Q(status='waiting', created_at__lte=cutoff_lobby) |
+    Q(~Q(status='finished'), created_at__lte=cutoff_finished)
+)
     channel_layer = get_channel_layer()
     print(f"Found stale games: {stale_games.count()}")
     for game in stale_games:
@@ -35,7 +37,6 @@ def reap_foresaken_waiting_games() -> None:
                 task_to_cancel = ACTIVE_GAMES[game.uid]['task']
                 task_to_cancel.cancel()
             ACTIVE_GAMES.pop(game.uid)
-
         game.delete()
     print(f"Deleted {stale_games.count()} stale games.")
     all_games = Game.objects.filter(status='waiting').all()
